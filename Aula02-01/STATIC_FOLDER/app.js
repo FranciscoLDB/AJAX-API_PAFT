791339cf5a05cb262d5611ce23cee07a71bc3ab2
franciscoldb <fluis905@gmail.com>
2023-02-01 20:49:00 -0300

    correcao exercicio

---
+++ Aula02-01/STATIC_FOLDER/app.js
@@ -0,0 +1,3 @@
+function createContact(){
+    console.log("create static");
+}
--- Aula11/crudTodo.py
+++ Aula11/crudTodo.py
@@ -1,43 +1,44 @@
 from flask import Flask, jsonify, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 db.init_app(app)
 
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(40), nullable=True)
-    phone = db.Column(db.String(40))
+    phone = db.Column(db.String(20), nullable=True)
 
     def __init__(self, name, phone):
         self.name = name
         self.phone = phone
-    
-contactsDB = db.Table(
-    "contacts",
-    db.Column("Contact_id", db.ForeignKey(Contact.id), primary_key=True),
-    db.Column("Contact_name", db.ForeignKey(Contact.name), primary_key=True),
-    db.Column("Contact_phone", db.ForeignKey(Contact.phone), primary_key=True),
-)
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+    def dict(self):
+        return {"id": self.id, "name": self.name, "phone": self.phone}
+
+with app.app_context():
+    db.create_all()    
 
 contactsList = [{'id': 1, 'name': 'John Doe',  'phone': '555-555-5555'},
             {'id': 2, 'name': 'Alice Silva',  'phone': '222-225-2222'} ]
 
 @app.route('/contact.html')
 def contact():
-    contacts = db.session.execute(db.select(Contact).order_by(Contact.id)).scalars()
+    contacts = Contact.query.all()
     return render_template('contact.html', contact=contacts)
 
 # GET request to retrieve all contacts
 @app.route('/contacts', methods=['GET'])
 def get_contacts(): 
-    if not contactsList:
-        return jsonify({'message':'No contacts founded in server'}), 404
-    return jsonify({'contacts': contactsList}), 200
+    contacts = Contact.query.all()
+    return jsonify({'contacts': [contact.as_dict() for contact in contacts]}), 200
 
 # GET request to retrieve one contacts
 @app.route('/contacts/<int:id>', methods=['get'])
@@ -55,11 +56,7 @@ def add_contact():
     data = request.get_json()
     if not data or not all(key in data for key in ('name','phone')):
         return jsonify({'message':'bad request'}), 400
-    id = 1
-    if len(contactsList) > 0:
-        id = contactsList[-1]['id']+1
-    c = {'id':id,'name':data['name'],'phone':data['phone']}
-    contactsList.append(c)
+    
 
     contact = Contact(
         name = data['name'],
@@ -67,9 +64,8 @@ def add_contact():
     )
     db.session.add(contact)
     db.session.commit()
-    print(contact.id)
 
-    return jsonify({'contact': c}), 201
+    return jsonify({'contact': contact.as_dict()}), 201
 
 # PUT request to update a contact
 @app.route('/contacts/<int:id>', methods=['PUT'])
@@ -83,18 +79,23 @@ def update_contact(id):
         if contact['id'] == id:
             contactsList[i] = {'id':id, 'name':data['name'],'phone':data['phone']}    
             return jsonify({'contact': contactsList[i]}),200
+
+    db.session.add(contact)
+    db.session.commit()
+    
     return jsonify({'message':'contact not found'}), 404
 
 # DELETE request to delete a contact
 @app.route('/contacts/<int:id>', methods=['DELETE'])
 def delete_contact(id):
-    for i,contact in enumerate(contactsList):
-        if contact['id'] == id:
-            del contactsList[i]   
-            return jsonify({'message': 'contact deleted'}),200
-    return jsonify({'message':'contact not found'}), 404
+    try:
+        contact = Contact.query.get_or_404(id)
+        db.session.delete(contact)
+        db.session.commit()        
+        return jsonify({'message':'contact deleted'}), 200
+    finally:
+        return jsonify({'message':'contact not found'}), 400
 
+    
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug = True)
+    app.run(debug=True)
